b28f0e3628560d1ab492a963b27113305909a2b7
Zulhumor <100398432+Zulhumor@users.noreply.github.com>
2022-04-02 00:33:01 -0700

    Add files via upload

---
+++ Practicing MySQL on Dataset Orders/module 6 - Orders.sql
@@ -0,0 +1,195 @@
+
+create database orders;
+USE orders;
+
+CREATE TABLE  AGENTS 
+   (	
+    AGENT_CODE CHAR(6) NOT NULL PRIMARY KEY, 
+	AGENT_NAME CHAR(40), 
+	WORKING_AREA CHAR(35), 
+	COMMISSION DOUBLE(10,2), 
+	PHONE_NO CHAR(15), 
+	COUNTRY VARCHAR(25) 
+	 );
+     
+     
+INSERT INTO AGENTS VALUES ('A007', 'Ramasundar', 'Bangalore', '0.15', '077-25814763', '')
+, ('A003', 'Alex ', 'London', '0.13', '075-12458969', '')
+, ('A008', 'Alford', 'New York', '0.12', '044-25874365', '')
+, ('A011', 'Ravi Kumar', 'Bangalore', '0.15', '077-45625874', '')
+, ('A010', 'Santakumar', 'Chennai', '0.14', '007-22388644', '')
+, ('A012', 'Lucida', 'San Jose', '0.12', '044-52981425', '')
+, ('A005', 'Anderson', 'Brisban', '0.13', '045-21447739', '')
+, ('A001', 'Subbarao', 'Bangalore', '0.14', '077-12346674', '')
+, ('A002', 'Mukesh', 'Mumbai', '0.11', '029-12358964', '')
+, ('A006', 'McDen', 'London', '0.15', '078-22255588', '')
+, ('A004', 'Ivan', 'Torento', '0.15', '008-22544166', '')
+, ('A009', 'Benjamin', 'Hampshair', '0.11', '008-22536178', '')
+;
+
+CREATE TABLE  CUSTOMER 
+   (	CUST_CODE VARCHAR(6) NOT NULL PRIMARY KEY, 
+	CUST_NAME VARCHAR(40) NOT NULL, 
+	CUST_CITY CHAR(35), 
+	WORKING_AREA VARCHAR(35) NOT NULL, 
+	CUST_COUNTRY VARCHAR(20) NOT NULL, 
+	GRADE INT, 
+	OPENING_AMT DOUBLE(12,2) NOT NULL, 
+	RECEIVE_AMT DOUBLE(12,2) NOT NULL, 
+	PAYMENT_AMT DOUBLE(12,2) NOT NULL, 
+	OUTSTANDING_AMT DOUBLE(12,2) NOT NULL, 
+	PHONE_NO VARCHAR(17) NOT NULL, 
+	AGENT_CODE CHAR(6) NOT NULL REFERENCES AGENTS
+);  
+
+INSERT INTO CUSTOMER VALUES 
+('C00013', 'Holmes', 'London', 'London', 'UK', '2', '6000.00', '5000.00', '7000.00', '4000.00', 'BBBBBBB', 'A003')
+, ('C00001', 'Micheal', 'New York', 'New York', 'USA', '2', '3000.00', '5000.00', '2000.00', '6000.00', 'CCCCCCC', 'A008')
+, ('C00020', 'Albert', 'New York', 'New York', 'USA', '3', '5000.00', '7000.00', '6000.00', '6000.00', 'BBBBSBB', 'A008')
+, ('C00025', 'Ravindran', 'Bangalore', 'Bangalore', 'India', '2', '5000.00', '7000.00', '4000.00', '8000.00', 'AVAVAVA', 'A011')
+, ('C00024', 'Cook', 'London', 'London', 'UK', '2', '4000.00', '9000.00', '7000.00', '6000.00', 'FSDDSDF', 'A006')
+, ('C00015', 'Stuart', 'London', 'London', 'UK', '1', '6000.00', '8000.00', '3000.00', '11000.00', 'GFSGERS', 'A003')
+, ('C00002', 'Bolt', 'New York', 'New York', 'USA', '3', '5000.00', '7000.00', '9000.00', '3000.00', 'DDNRDRH', 'A008')
+, ('C00018', 'Fleming', 'Brisban', 'Brisban', 'Australia', '2', '7000.00', '7000.00', '9000.00', '5000.00', 'NHBGVFC', 'A005')
+, ('C00021', 'Jacks', 'Brisban', 'Brisban', 'Australia', '1', '7000.00', '7000.00', '7000.00', '7000.00', 'WERTGDF', 'A005')
+, ('C00019', 'Yearannaidu', 'Chennai', 'Chennai', 'India', '1', '8000.00', '7000.00', '7000.00', '8000.00', 'ZZZZBFV', 'A010')
+, ('C00005', 'Sasikant', 'Mumbai', 'Mumbai', 'India', '1', '7000.00', '11000.00', '7000.00', '11000.00', '147-25896312', 'A002')
+, ('C00007', 'Ramanathan', 'Chennai', 'Chennai', 'India', '1', '7000.00', '11000.00', '9000.00', '9000.00', 'GHRDWSD', 'A010')
+, ('C00022', 'Avinash', 'Mumbai', 'Mumbai', 'India', '2', '7000.00', '11000.00', '9000.00', '9000.00', '113-12345678','A002')
+, ('C00004', 'Winston', 'Brisban', 'Brisban', 'Australia', '1', '5000.00', '8000.00', '7000.00', '6000.00', 'AAAAAAA', 'A005')
+, ('C00023', 'Karl', 'London', 'London', 'UK', '0', '4000.00', '6000.00', '7000.00', '3000.00', 'AAAABAA', 'A006')
+, ('C00006', 'Shilton', 'Torento', 'Torento', 'Canada', '1', '10000.00', '7000.00', '6000.00', '11000.00', 'DDDDDDD', 'A004')
+, ('C00010', 'Charles', 'Hampshair', 'Hampshair', 'UK', '3', '6000.00', '4000.00', '5000.00', '5000.00', 'MMMMMMM', 'A009')
+, ('C00017', 'Srinivas', 'Bangalore', 'Bangalore', 'India', '2', '8000.00', '4000.00', '3000.00', '9000.00', 'AAAAAAB', 'A007')
+, ('C00012', 'Steven', 'San Jose', 'San Jose', 'USA', '1', '5000.00', '7000.00', '9000.00', '3000.00', 'KRFYGJK', 'A012')
+, ('C00008', 'Karolina', 'Torento', 'Torento', 'Canada', '1', '7000.00', '7000.00', '9000.00', '5000.00', 'HJKORED', 'A004')
+, ('C00003', 'Martin', 'Torento', 'Torento', 'Canada', '2', '8000.00', '7000.00', '7000.00', '8000.00', 'MJYURFD', 'A004')
+, ('C00009', 'Ramesh', 'Mumbai', 'Mumbai', 'India', '3', '8000.00', '7000.00', '3000.00', '12000.00', 'Phone No', 'A002')
+, ('C00014', 'Rangarappa', 'Bangalore', 'Bangalore', 'India', '2', '8000.00', '11000.00', '7000.00', '12000.00', 'AAAATGF', 'A001')
+, ('C00016', 'Venkatpati', 'Bangalore', 'Bangalore', 'India', '2', '8000.00', '11000.00', '7000.00', '12000.00', 'JRTVFDD', 'A007')
+, ('C00011', 'Sundariya', 'Chennai', 'Chennai', 'India', '3', '7000.00', '11000.00', '7000.00', '11000.00', 'PPHGRTS', 'A010');
+
+CREATE TABLE  ORDERS 
+   (
+    ORD_NUM DOUBLE(6,0) NOT NULL PRIMARY KEY, 
+	ORD_AMOUNT DOUBLE(12,2) NOT NULL, 
+	ADVANCE_AMOUNT DOUBLE(12,2) NOT NULL, 
+	ORD_DATE DATE NOT NULL, 
+	CUST_CODE VARCHAR(6) NOT NULL REFERENCES CUSTOMER, 
+	AGENT_CODE CHAR(6) NOT NULL REFERENCES AGENTS, 
+	ORD_DESCRIPTION VARCHAR(60) NOT NULL
+   );
+   
+    INSERT INTO ORDERS VALUES
+   ('200100', '1000.00', '600.00', '2008-08-01', 'C00013', 'A003', 'SOD')
+, ('200110', '3000.00', '500.00', '2008-04-15', 'C00019', 'A010', 'SOD')
+,('200107', '4500.00', '900.00', '2008-08-30', 'C00007', 'A010', 'SOD')
+,('200112', '2000.00', '400.00', '2008-05-30', 'C00016', 'A007', 'SOD')
+,('200113', '4000.00', '600.00', '2008-06-10', 'C00022', 'A002', 'SOD')
+,('200102', '2000.00', '300.00', '2008-05-25', 'C00012', 'A012', 'SOD')
+,('200114', '3500.00', '2000.00', '2008-08-15', 'C00002', 'A008', 'SOD')
+,('200122', '2500.00', '400.00', '2008-09-16', 'C00003', 'A004', 'SOD')
+,('200118', '500.00', '100.00', '2008-07-20', 'C00023', 'A006', 'SOD')
+,('200119', '4000.00', '700.00', '2008-09-16', 'C00007', 'A010', 'SOD')
+,('200121', '1500.00', '600.00', '2008-09-23', 'C00008', 'A004', 'SOD')
+,('200130', '2500.00', '400.00', '2008-07-30', 'C00025', 'A011', 'SOD')
+,('200134', '4200.00', '1800.00', '2008-09-25', 'C00004', 'A005', 'SOD')
+,('200108', '4000.00', '600.00', '2008-02-15', 'C00008', 'A004', 'SOD')
+,('200103', '1500.00', '700.00', '2008-05-15', 'C00021', 'A005', 'SOD')
+,('200105', '2500.00', '500.00', '2008-07-18', 'C00025', 'A011', 'SOD')
+,('200109', '3500.00', '800.00', '2008-07-30', 'C00011', 'A010', 'SOD')
+,('200101', '3000.00', '1000.00', '2008-07-15', 'C00001', 'A008', 'SOD')
+,('200111', '1000.00', '300.00', '2008-07-10', 'C00020', 'A008', 'SOD')
+,('200104', '1500.00', '500.00', '2008-03-13', 'C00006', 'A004', 'SOD')
+,('200106', '2500.00', '700.00', '2008-04-20', 'C00005', 'A002', 'SOD')
+,('200125', '2000.00', '600.00', '2008-10-10', 'C00018', 'A005', 'SOD')
+,('200117', '800.00', '200.00', '2008-10-20', 'C00014', 'A001', 'SOD')
+,('200123', '500.00', '100.00', '2008-09-16', 'C00022', 'A002', 'SOD')
+,('200120', '500.00', '100.00', '2008-07-20', 'C00009', 'A002', 'SOD')
+,('200116', '500.00', '100.00', '2008-07-13', 'C00010', 'A009', 'SOD')
+,('200124', '500.00', '100.00', '2008-06-20', 'C00017', 'A007', 'SOD')
+,('200126', '500.00', '100.00', '2008-06-24', 'C00022', 'A002', 'SOD')
+,('200129', '2500.00', '500.00', '2008-07-20', 'C00024', 'A006', 'SOD')
+,('200127', '2500.00', '400.00', '2008-07-20', 'C00015', 'A003', 'SOD')
+,('200128', '3500.00', '1500.00', '2008-07-20', 'C00009', 'A002', 'SOD')
+,('200135', '2000.00', '800.00', '2008-09-16', 'C00007', 'A010', 'SOD')
+,('200131', '900.00', '150.00', '2008-08-26', 'C00012', 'A012', 'SOD')
+,('200133', '1200.00', '400.00', '2008-06-29', 'C00009', 'A002', 'SOD');
+
+
+-- 1. Write a SQL query to calculate average purchase amount of all orders. Return average purchase amount
+
+SELECT avg(ord_amount) FROM Orders;
+
+-- 2. Write a SQL query to find the highest grade of the customers for each of the city. Return city, maximum grade
+
+SELECT cust_city, MAX(grade) FROM customer
+group by cust_city;
+
+-- 3. Write a SQL query to find the lowest commission of agent in city London
+
+SELECT MIN(commission), agent_name, working_area
+FROM agents 
+WHERE working_area='London';
+
+-- 4. Find all customers that had agents. Select customer name, agent name and agent phone number. 
+
+SELECT c.cust_name, a.agent_name, a.phone_no
+FROM agents a
+JOIN customer c ON c.agent_code = a.agent_code;
+
+
+-- 5. Write a SQL query (from table orders) to find highest order (purchase) amount by each customer in a particular order date.
+-- Filter the result by highest order (purchase) amount above 2000.00. Return customer id, order date and maximum purchase amount.
+
+SELECT MAX(o.ord_amount) AS Highest_order, c.cust_name, o.ord_date
+FROM orders o 
+JOIN customer c ON c.agent_code = o.agent_code
+GROUP BY o.ord_date, c.cust_name
+HAVING MAX(o.ord_amount)>2000; 
+ 
+-- 6. Write a SQL query to find those orders where order amount exists between 500 and 2000. Return ord_no, purch_amt, cust_name, city
+
+SELECT o.ord_num, o.ord_amount, c.cust_name, c.cust_city
+FROM orders o 
+JOIN customer c ON c.agent_code = o.agent_code
+WHERE o.ord_amount BETWEEN 500 AND 2000;
+
+-- 7. Write a SQL statement to make a report with customer name, city, order number, order date, and order amount 
+-- 		in ascending order according to the order date to find that either any of the existing customers 
+-- 		have placed no order or placed one or more orders
+
+SELECT c.cust_name, c.cust_city, o.ord_num, o.ord_date, o.ord_amount,
+		CASE 
+			WHEN 	THEN
+		END
+FROM orders o
+JOIN customer c ON c.agent_code = o.agent_code        
+ORDER BY o.ord_date ASC;
+
+
+-- 8. From tables customer and orders, write a SQL query to find all the orders issued by the salesman 'Martin'. 
+-- 		Return ord_no, purch_amt, ord_date, customer_id and salesman_id
+
+SELECT o.ord_num, o.ord_amount, o.ord_date, c.cust_code, a.agent_code
+FROM orders o 
+INNER JOIN customer c ON c.agent_code = o.agent_code
+INNER JOIN agents a ON a.agent_code = o.agent_code
+WHERE a.agent_name = 'Martin';
+
+
+-- 9. Find average ord_amount by customer country. Show case a list of all customer names, country, their ord_amount together 
+-- 		with average ord_amount by customer country.
+
+WITH Avg_ord_amount AS(
+	SELECT c.agent_code, AVG(o.ord_amount) as average_ord_amount
+    FROM orders o 
+    INNER JOIN customer c ON c.agent_code = o.agent_code
+    group by cust_country
+)
+SELECT c.cust_name, c.cust_country, o.ord_amount, aoa.average_ord_amount
+FROM  customer c
+INNER JOIN orders o ON o.agent_code = c.agent_code
+INNER JOIN Avg_ord_amount aoa ON aoa.agent_code = c.agent_code
+Group BY c.cust_country;
+
